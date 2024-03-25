5e69752aae9c5ddcc997eb2904f1f14a821056ff
Hashu-CH <calebhsu@outlook.com>
2024-03-25 16:11:50 -0700

    Retrieve data from the mySQL database and process it to be displayed on the static front page

--- app.py
+++ app.py
@@ -1,27 +1,81 @@
 import json
 from flask import Flask, request, render_template, jsonify
+import mysql.connector
 from threading import Thread
 app = Flask(__name__)
 
+# db connection details change later depending on how ronald configs 
+db_name = "db_name"
+username = "username"
+password = "password"
+
+#example db schema 
+# id 
+# time taken time/day/month/year
+# people
+# total chair 
+# taken chair
+# total table 
+# taken table 
+
+# Issue - need to implement some thing to only pull the mot recent information 
+# otherwise we would lose PAST data 
+data = dict()
+
+# Database connection details (replace with yours)
+db_name = "your_database_name"
+username = "your_username"
+password = "your_password"
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=db_name
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM occupancy ORDER BY created_at DESC LIMIT 3")
+    cursor.fetchall()
+    # (data) should be a dictionary formatted -> 'location name' = (dict) column name : column information
+    # it would be nice to have a time taken in the data base 
+    cols = [description[0] for description in cursor.description]
+    for row in cursor: 
+        location = row[0]
+        location_info = dict(zip(cols[1:], row[1:])) 
+        data[location] = location_info
+
+except mysql.connector.Error as err:
+    print("Error connecting to database:", err)
+
+finally:
+    if connection:
+        connection.close()
+    print("Connection closed.")
 
 
-#default values
-total_population = 42
-west_population = 35
-east_population = 7
+# the rendered template takes a dictionary of element id's and values 
+# e.g. {{ population }}  --> value for population will be put in that space 
+# THERE IS PROBABLY A BETTER WAY TO DO THIS BUT IT WOULD REQUIRE MORE COMPLEX FLASK
+        # total population
+        # population, open tables and chairs on each floor 
+context = {}
+context['total_population'] = data[1]['population'] + data[2]['population'] + data[3]['population']
+for floor in (1, 2, 3):
+    open_tables = data[floor]['total_tables'] - data[floor]['taken_tables']
+    open_chairs = data[floor]['total_chairs'] - data[floor]['taken_chairs']
+    context['floor_' + str(floor) + '_otables'] = open_tables
+    context['floor_' + str(floor) + '_ochairs'] = open_chairs
+
+# access in html template as floor_#_otables 
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def display_home():
-    global total_population, west_population, east_population
     if request.method == 'GET':
-        return render_template('index.html', total_population = total_population, west_population = west_population, east_population = east_population) ## some var
+        return render_template('index.html', **context) 
     else:
-        updated_data = json.loads(request.get_json())
-        total_population = 42
-        west_population = 35
-        east_population = 7
-        return jsonify("successfully updated data")
+        pass # idk what to put here 
         
 
 
